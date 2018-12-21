fd3d958f0b86eb8e22ecd38506c6c10bf912a078
604234037CS <604234037@parichat.skru.ac.th>
2018-12-21 16:55:31 +0700

    chareef037

--- lab3/src/pages/home/home.ts
+++ lab3/src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { AboutPage } from './../about/about';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -9,6 +10,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  gotoPage(){
+    this.navCtrl.push(AboutPage);
   }
 
 }
